84ab2de2cad663cf61c8818be539f989e26c7459
amargota <33738610+amargota@users.noreply.github.com>
2018-01-10 00:14:14 +0800

    Models
    Check meytang customer.cs

--- PVMTrading_v1/Models/Customer.cs
+++ PVMTrading_v1/Models/Customer.cs
@@ -10,8 +10,52 @@ namespace PVMTrading_v1.Models
 
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
 
+        public string MiddleName { get; set; }
 
+        public string LastName { get; set; }
+
+        public string NameExtension { get; set; }
+
+        public int Mobile { get; set; }
+
+        public int Telephone { get; set; }
+
+        public string Email { get; set; }
+
+        public int Birthdate { get; set; }
+
+        public int CivilStatusid { get; set; }
+
+        public int Sexid { get; set; }
+
+        public int DateCreated { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+
+        public string Nationality { get; set; }
+
+        public int TaxidentificationNumber { get; set; }
+
+        public string UnitRoomFloor { get; set; }
+
+        public string BuildingName { get; set; }
+
+        public int LotBlockHouseNumber { get; set; }
+
+        public string Street { get; set; }
+
+        public string SubdivisionVillage { get; set; }
+
+        public string Barangay { get; set; }
+
+        public string CityMunicipality { get; set; }
+
+        public string Province { get; set; }
+
+        public string Country { get; set; }
+
+        public int ZipCode { get; set; }
     }
 }
