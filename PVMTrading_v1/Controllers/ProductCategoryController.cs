5a3e9fd8f60c6bc227fc82db7ba0c972307eeceb
dev-angelica-payawal <dev.angelica.payawal@gmail.com>
2018-01-19 13:36:55 +0800

    product category controller

---
+++ PVMTrading_v1/Controllers/ProductCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVMTrading_v1.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        // GET: ProductCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
