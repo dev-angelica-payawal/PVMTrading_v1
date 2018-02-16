a8f801b4a871768095eb76e24a0cef04ad4abdb3
dev-angelica-payawal <dev.angelica.payawal@gmail.com>
2018-02-16 20:38:41 +0800

    product duplication need fix

---
+++ PVMTrading_v1/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PVMTrading_v1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace PVMTrading_v1.Controllers
+{
+
+
+    public class UsersController : Controller
+    {
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext _context;
+
+        public UsersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Users
+        public ActionResult Index()
+        {
+            
+            
+            return View();
+        }
+        
+    }
+}
--- PVMTrading_v1/ViewModels/UsersViewModel.cs
+++ PVMTrading_v1/ViewModels/UsersViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
+using PVMTrading_v1.Models;
 
 namespace PVMTrading_v1.ViewModels
 {
     public class UsersViewModel
     {
-
+        public ApplicationUser Users { set; get; }
+        public List<string> Roles { set; get; }
     }
 }
